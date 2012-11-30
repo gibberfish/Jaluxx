7373f87e6a38c29481305094e0e83629b4b87a0e
Mark on LAPPY <growler1970@gmail.com>
2012-11-30 21:28:15 +0000

    .

--- src/main/java/com/mindbadger/jaluxx/card/CardType.java
+++ src/main/java/com/mindbadger/jaluxx/card/CardType.java
@@ -1,5 +1,18 @@
 package com.mindbadger.jaluxx.card;
 
 public enum CardType {
-	ACTION, KEEPER, RULE, GOAL;
+	ACTION ("Action"),
+	KEEPER ("Keeper"),
+	RULE ("Rule"),
+	GOAL ("Goal");
+	
+	private String cardTypeText;
+	
+	private CardType (String cardTypeText) {
+		this.cardTypeText = cardTypeText;
+	}
+	
+	public String getText () {
+		return cardTypeText;
+	}
 }
